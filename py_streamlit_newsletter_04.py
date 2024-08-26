07ad5695112aacac72924fbb2a9ffe6a83d9abf5
timurna <62594928+timurna@users.noreply.github.com>
2024-08-26 11:48:45 +0200

    Update py_streamlit_newsletter_04.py

--- py_streamlit_newsletter_04.py
+++ py_streamlit_newsletter_04.py
@@ -63,14 +63,14 @@ def set_mobile_css():
 # Glossary content
 glossary = {
     # Score Metrics
-    '**Score Metrics**' '',
+    '**Score Metrics**': '',  # Empty string as value to remove the colon
     'Offensive Score': 'A score representing a player\'s overall offensive performance.',
     'Defensive Score': 'A score representing a player\'s overall defensive performance.',
     'Physical Offensive Score': 'A score representing a player\'s physical contributions to offensive play.',
     'Physical Defensive Score': 'A score representing a player\'s physical contributions to defensive play.',
     
     # Offensive Metrics
-    '**Offensive Metrics**' '',
+    '**Offensive Metrics**': '',  # Empty string as value to remove the colon
     'Take on into the Box': 'Number of successful dribbles into the penalty box.',
     'TouchOpBox': 'Number of touches in the opponent\'s penalty box.',
     'KeyPass': 'Passes that directly lead to a shot on goal.',
@@ -100,12 +100,15 @@ glossary = {
     'Shot conversion': 'Percentage of shots converted to goals.',
     'Ast': 'Assists.',
     'xA': 'Expected assists.',
+    
+    # Additional Metrics
+    '**Additional Metrics**': '',  # Empty string as value to remove the colon
     'OnTarget%': 'Percentage of shots on target out of total shots.',
     'TcklMade%': 'Percentage of tackles successfully made out of total tackle attempts.',
     'Pass%': 'Percentage of completed passes out of total passes attempted.',
     
     # Defensive Metrics
-    '**Defensive Metrics**' '',
+    '**Defensive Metrics**': '',  # Empty string as value to remove the colon
     'TcklAtt': 'Tackles attempted.',
     'Tckl': 'Tackles made.',
     'AdjTckl': 'Adjusted tackles, considering context.',
@@ -116,7 +119,7 @@ glossary = {
     'Clrnce': 'Clearances made.',
     
     # Physical Metrics
-    '**Physical Metrics**' '',
+    '**Physical Metrics**': '',  # Empty string as value to remove the colon
     'Distance': 'Total distance covered by the player during the match.',
     'M/min': 'Meters covered per minute by the player.',
     'HSR Distance': 'High-speed running distance covered.',

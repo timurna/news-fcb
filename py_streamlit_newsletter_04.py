7f23a11dff9c450d2f3e38869058d0efe79419c1
timurna <62594928+timurna@users.noreply.github.com>
2024-08-26 15:08:53 +0200

    Update py_streamlit_newsletter_04.py

--- py_streamlit_newsletter_04.py
+++ py_streamlit_newsletter_04.py
@@ -148,7 +148,7 @@ glossary = {
 }
 
 # Load the dataset from Parquet
-file_path = 'https://raw.githubusercontent.com/timurna/news-fcb/main/test-neu.parquet'
+file_path = 'https://raw.githubusercontent.com/timurna/news-fcb/main/test-neu-upd.parquet'
 data = pd.read_parquet(file_path)
 
 # Calculate age from birthdate

a034b5b6ffc483850237da0f6f4a43824c84ccb7
timurna <62594928+timurna@users.noreply.github.com>
2024-08-28 16:45:49 +0200

    Update py_streamlit_newsletter_04.py

--- py_streamlit_newsletter_04.py
+++ py_streamlit_newsletter_04.py
@@ -277,15 +277,15 @@ else:
         col1, col2, col3 = st.columns([1, 1, 1])
 
         with col1:
-            leagues = sorted(data['Competition'].unique())  # Sort leagues alphabetically
+            leagues = sorted(data['League'].unique())  # Sort leagues alphabetically
             selected_league = st.selectbox("Select League", leagues, key="select_league")
 
         with col2:
-            league_data = data[data['Competition'] == selected_league]
+            league_data = data[data['League'] == selected_league]
 
             # Week Summary and Matchday Filtering Logic
-            week_summary = league_data.groupby(['Competition', 'Week']).agg({'Date.1': ['min', 'max']}).reset_index()
-            week_summary.columns = ['Competition', 'Week', 'min', 'max']
+            week_summary = league_data.groupby(['League', 'Week']).agg({'Date.1': ['min', 'max']}).reset_index()
+            week_summary.columns = ['League', 'Week', 'min', 'max']
 
             week_summary['min'] = pd.to_datetime(week_summary['min'])
             week_summary['max'] = pd.to_datetime(week_summary['max'])
@@ -294,7 +294,7 @@ else:
                 lambda row: f"{row['Week']} ({row['min'].strftime('%d.%m.%Y')} - {row['max'].strftime('%d.%m.%Y')})", axis=1
             )
 
-            filtered_weeks = week_summary[week_summary['Competition'] == selected_league].sort_values(by='min').drop_duplicates(subset=['Week'])
+            filtered_weeks = week_summary[week_summary['League'] == selected_league].sort_values(by='min').drop_duplicates(subset=['Week'])
 
             matchday_options = filtered_weeks['Matchday'].tolist()
             selected_matchday = st.selectbox("Select Matchday", matchday_options, key="select_matchday")
@@ -307,7 +307,7 @@ else:
 
     # Filter the data by the selected position group
     league_and_position_data = data[
-        (data['Competition'] == selected_league) &
+        (data['League'] == selected_league) &
         (data['Week'] == selected_week) &
         (data['Position Groups'].apply(lambda groups: selected_position_group in groups))
     ]

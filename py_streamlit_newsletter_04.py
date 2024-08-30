fba18d99826cbfdc076d262b10033a41cbca6465
timurna <62594928+timurna@users.noreply.github.com>
2024-08-30 15:49:12 +0200

    Update py_streamlit_newsletter_04.py

--- py_streamlit_newsletter_04.py
+++ py_streamlit_newsletter_04.py
@@ -323,47 +323,56 @@ else:
     with st.container():
         tooltip_headers = {metric: glossary.get(metric, '') for metric in ['Offensive Score', 'Defensive Score', 'Physical Offensive Score', 'Physical Defensive Score', 'Goal Threat Score'] + physical_metrics + offensive_metrics + defensive_metrics}
 
-        def display_metric_tables(metrics_list, title):
-            with st.expander(title, expanded=False):  # Setting expanded=False to keep it closed by default
-                for metric in metrics_list:
-                    if metric not in league_and_position_data.columns:
-                        st.write(f"Metric {metric} not found in the data")
-                        continue
+    def display_metric_tables(metrics_list, title):
+        with st.expander(title, expanded=False):  # Setting expanded=False to keep it closed by default
+            for metric in metrics_list:
+                if metric not in league_and_position_data.columns:
+                    st.write(f"Metric {metric} not found in the data")
+                continue
 
-                    league_and_position_data[metric] = pd.to_numeric(league_and_position_data[metric], errors='coerce')
+            league_and_position_data[metric] = pd.to_numeric(league_and_position_data[metric], errors='coerce')
 
-                    # Round the Age column to ensure no decimals
-                    league_and_position_data['Age'] = league_and_position_data['Age'].round(0).astype(int)
+            # Round the Age column to ensure no decimals
+            league_and_position_data['Age'] = league_and_position_data['Age'].round(0).astype(int)
 
-                    top10 = league_and_position_data[['playerFullName', 'Age', 'newestTeam', 'Position_x', metric]].dropna(subset=[metric]).sort_values(by=metric, ascending=False).head(10)
+            # Include the 'Min' column and rename it to 'Min.'
+            top10 = league_and_position_data[['playerFullName', 'Age', 'Min', 'newestTeam', 'Position_x', metric]].dropna(subset=[metric]).sort_values(by=metric, ascending=False).head(10)
 
-                    if top10.empty:
-                        st.header(f"Top 10 Players in {metric}")
-                        st.write("No data available")
-                    else:
-                        # Reset the index to create a rank column starting from 1
-                        top10.reset_index(drop=True, inplace=True)
-                        top10.index += 1
-                        top10.index.name = 'Rank'
+            # Rename the columns in one place
+            top10.rename(columns={
+                'playerFullName': 'Player',
+                'newestTeam': 'Team',
+                'Position_x': 'Position',
+                'Min': 'Min.'
+                }, inplace=True)
 
-                        # Ensure the Rank column is part of the DataFrame before styling
-                        top10 = top10.reset_index()
+            if top10.empty:
+                    st.header(f"Top 10 Players in {metric}")
+                    st.write("No data available")
+            else:
+                    # Reset the index to create a rank column starting from 1
+                    top10.reset_index(drop=True, inplace=True)
+                    top10.index += 1
+                    top10.index.name = 'Rank'
 
-                        st.markdown(f"<h2>{metric}</h2>", unsafe_allow_html=True)
-                        top10.rename(columns={'playerFullName': 'Player', 'newestTeam': 'Team', 'Position_x': 'Position'}, inplace=True)
-                        top10[metric] = top10[metric].apply(lambda x: f"{x:.2f}")
+                    # Ensure the Rank column is part of the DataFrame before styling
+                    top10 = top10.reset_index()
 
-                        def color_row(row):
-                            return ['background-color: #d4edda' if row['Age'] < 24 else '' for _ in row]
+                    st.markdown(f"<h2>{metric}</h2>", unsafe_allow_html=True)
+                    top10[metric] = top10[metric].apply(lambda x: f"{x:.2f}")
 
-                        top10_styled = top10.style.apply(color_row, axis=1)
-                        top10_html = top10_styled.to_html()
+            def color_row(row):
+                    return ['background-color: #d4edda' if row['Age'] < 24 else '' for _ in row]
 
-                        for header, tooltip in tooltip_headers.items():
-                            if tooltip:
-                                top10_html = top10_html.replace(f'>{header}<', f'><span class="tooltip">{header}<span class="tooltiptext">{tooltip}</span></span><')
+            top10_styled = top10.style.apply(color_row, axis=1)
+            top10_html = top10_styled.to_html()
 
-                        st.write(top10_html, unsafe_allow_html=True)
+            for header, tooltip in tooltip_headers.items():
+                    if tooltip:
+                        top10_html = top10_html.replace(f'>{header}<', f'><span class="tooltip">{header}<span class="tooltiptext">{tooltip}</span></span><')
+
+            st.write(top10_html, unsafe_allow_html=True)
+
 
         display_metric_tables(['Offensive Score', 'Goal Threat Score', 'Defensive Score', 'Physical Offensive Score', 'Physical Defensive Score'], "Score Metrics")
         display_metric_tables(physical_metrics, "Physical Metrics")
